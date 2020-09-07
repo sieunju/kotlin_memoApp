f4aee436f56b429452814b59f8d6139c23f00e8b
j.sieun73 <j.sieun73@gmail.com>
2020-09-07 18:03:10 +0900

    2020.09.07 - 이미지 로더 썸네일 옵션 추가.

--- app/src/main/java/com/hmju/memo/ui/bindingadapter/GlideBindingAdapter.kt
+++ app/src/main/java/com/hmju/memo/ui/bindingadapter/GlideBindingAdapter.kt
@@ -40,7 +40,7 @@ fun bindingImg(
         Glide.with(imgView.context)
             .load(getUrl(it))
             .placeholder(placeHolder[ran])
-            .override(200,200)
+            .thumbnail(0.1F)
             .transform(CenterCrop())
             .into(imgView)
     }
@@ -66,27 +66,6 @@ fun bindingImgHeader(
             .dontAnimate()
             .transform(CenterCrop())
             .error(R.drawable.ic_profile_default)
-            .into(imgView)
-    }
-}
-
-@BindingAdapter("memoDetailImg")
-fun bindingImgDetail(
-    imgView: AppCompatImageView,
-    imgUrl : String?
-) {
-    val placeHolder = listOf<Int>(
-        R.color.colorPlaceHolder_1,
-        R.color.colorPlaceHolder_2,
-        R.color.colorPlaceHolder_3,
-        R.color.colorPlaceHolder_4)
-    val ran = Random().nextInt(placeHolder.size)
-
-    imgUrl?.let {
-        Glide.with(imgView.context)
-            .load(getUrl(it))
-            .placeholder(placeHolder[ran])
-            .transform(CenterCrop())
             .into(imgView)
     }
 }
