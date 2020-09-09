3045c7ad5a5aa9ce8687f33c2bb71501e3001ec7
j.sieun73 <j.sieun73@gmail.com>
2020-09-09 18:00:41 +0900

    2020.09.09 수정사항 - 금일 작업 내역 반영

--- app/src/main/java/com/hmju/memo/ui/toast/CustomToast.kt
+++ app/src/main/java/com/hmju/memo/ui/toast/CustomToast.kt
@@ -1,6 +1,11 @@
 package com.hmju.memo.ui.toast
 
+import android.app.Activity
+import android.view.Gravity
+import android.view.ViewGroup
 import android.widget.Toast
+import com.hmju.memo.R
+import kotlinx.android.synthetic.main.toast_view.view.*
 
 /**
  * Description :
@@ -8,6 +13,14 @@ import android.widget.Toast
  * Created by hmju on 2020-09-09
  */
 
-inline fun Toast.showToast(text: String, duration: Int) {
-    
+inline fun Toast.showToast(act : Activity,text: String, duration: Int) {
+    val inflater = act.layoutInflater
+    val view = inflater.inflate(R.layout.toast_view,act as ViewGroup)
+    view.tvToast.text = text
+
+    val toast = Toast(act)
+    toast.setGravity(Gravity.CENTER,0,0)
+    toast.duration = duration
+    toast.view = view
+    toast.show()
 }
