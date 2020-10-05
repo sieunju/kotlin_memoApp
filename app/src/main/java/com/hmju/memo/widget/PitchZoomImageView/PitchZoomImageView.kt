2c532c5ccb50e1e860c1d5509de087e1d9c67446
Sieun Ju <j.sieun73@gmail.com>
2020-10-06 00:07:34 +0900

    2020. 10. 04서5. 수정사항 - Pitch Zoom ImageView 클래스 생

---
+++ app/src/main/java/com/hmju/memo/widget/PitchZoomImageView/PitchZoomImageView.kt
@@ -0,0 +1,16 @@
+package com.hmju.memo.widget.PitchZoomImageView
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+/**
+ * Description : Pitch To Zoom ImageView Class
+ *
+ * Created by juhongmin on 2020/10/05
+ */
+class PitchZoomImageView(private val ctx: Context, private val attrs: AttributeSet?) : AppCompatImageView(ctx, attrs) {
+
+
+
+}
